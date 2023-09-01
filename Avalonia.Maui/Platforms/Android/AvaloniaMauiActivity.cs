479ac763f08c31156da64ad3ef5fd952d211cd06
Jens-Uwe Jährig <jens-uwe.jaehrig@fsd-web.de>
2023-09-01 18:07:45 +0200

    update to net7 to circumvent crazy maui-tizen missing bug from xamarin

--- Avalonia.Maui/Platforms/Android/AvaloniaMauiActivity.cs
+++ Avalonia.Maui/Platforms/Android/AvaloniaMauiActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Avalonia.Android;
 
 namespace Avalonia.Maui.Platforms.Android
@@ -7,6 +8,8 @@ namespace Avalonia.Maui.Platforms.Android
     public class AvaloniaMauiActivity : MauiAppCompatActivity, IActivityResultHandler
     {
         public Action<int, Result, Intent> ActivityResult { get; set; }
+        public Action<int, string[], Permission[]> RequestPermissionsResult { get ; set ; }
+
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
