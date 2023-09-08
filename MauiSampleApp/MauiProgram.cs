157e066d06d1dd14c63ffc72c63d579c5641c82a
JENS-PC\jens <jaehrig.jens@gmail.com>
2023-09-08 16:38:27 +0200

    rename extension from xaml to mxaml for maui xaml files, to workaroud the ava source gen issue https://github.com/AvaloniaUI/AvaloniaMauiHybrid/issues/7#issuecomment-1703177240

--- MauiSampleApp/MauiProgram.cs
+++ MauiSampleApp/MauiProgram.cs
@@ -3,11 +3,24 @@ using Avalonia.Maui;
 using Avalonia.Maui.Controls;
 using Avalonia.Maui.Handlers;
 using MauiSample;
+using System.Collections.Immutable;
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/maui", "Microsoft.Maui.Controls", AssemblyName = "Microsoft.Maui.Controls")]
 
 namespace MauiSampleApp
 {
+
     public static class MauiProgram
     {
+        private static object DoNotOptimiceAway = ImmutableArray.Create(
+        
+            typeof(Microsoft.Maui.Controls.Shell),
+            typeof(Microsoft.Maui.Controls.ContentPage),
+            typeof(Microsoft.Maui.Controls.Application),
+            typeof(Microsoft.Maui.Controls.ResourceDictionary)
+        );
+
+
         public static Microsoft.Maui.Hosting.MauiApp CreateMauiApp()
         {
             var builder = Microsoft.Maui.Hosting.MauiApp.CreateBuilder();
