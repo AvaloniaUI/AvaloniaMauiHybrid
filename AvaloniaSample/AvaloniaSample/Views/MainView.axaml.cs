08ed3dccab2ed907a199b9710c59dc7f11fa496b
Max Katz <maxkatz6@outlook.com>
2023-09-20 17:53:38 -0700

    Update sample

--- AvaloniaSample/AvaloniaSample/Views/MainView.axaml.cs
+++ AvaloniaSample/AvaloniaSample/Views/MainView.axaml.cs
@@ -1,5 +1,9 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Maui.Controls;
+using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
+using Syncfusion.Maui.ProgressBar;
 
 namespace AvaloniaSample.Views;
 
@@ -8,5 +12,15 @@ public partial class MainView : UserControl
     public MainView()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void MediaElement_OnPositionChanged(object? sender, MediaPositionChangedEventArgs e)
+    {
+        var mediaElement = (MediaElement)sender!;
+        var progressBar = (SfCircularProgressBar)this.Get<MauiControlHost>("ProgressBarHost").Content!;
+        progressBar.IsIndeterminate = false;
+        progressBar.Minimum = 0;
+        progressBar.Maximum = 1;
+        progressBar.Progress = e.Position / mediaElement.Duration;
     }
 }
