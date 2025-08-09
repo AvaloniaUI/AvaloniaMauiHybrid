f3619c501c46b91a6ddd06ff02d752fed362ea7b
cedri <cedric.boutin56@gmail.com>
2025-08-09 21:21:09 +0200

    Clean up

--- Avalonia.Maui/Platforms/Windows/AppActionsPatch.cs
+++ Avalonia.Maui/Platforms/Windows/AppActionsPatch.cs
@@ -1,14 +1,15 @@
 ﻿#if WINDOWS10_0_19041_0_OR_GREATER
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Shapes;
 using Microsoft.Maui.ApplicationModel;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Avalonia.Maui.Platforms.Windows
 {
@@ -46,25 +47,30 @@ namespace Avalonia.Maui.Platforms.Windows
                         var arg = args.FirstOrDefault(a => a.StartsWith(AppActionPrefix));
                         if (arg != null)
                         {
-                            IntPtr lpData = Marshal.StringToHGlobalUni(arg);
-
-                            var cds = new COPYDATASTRUCT
-                            {
-                                dwData = IntPtr.Zero, // you can put your message type here
-                                cbData = (arg.Length) * 2, // Unicode bytes, + null terminator
-                                lpData = lpData
-                            };
-
-                            SendMessage(p.MainWindowHandle, WM_COPYDATA, IntPtr.Zero, ref cds);
-
-                            Marshal.FreeHGlobal(lpData);
-
-                            Environment.Exit(0);
+                            SendMessage(p.MainWindowHandle, arg);
                         }                        
                     }
                 }
             }
 
+        }
+
+        private static void SendMessage(IntPtr handle, string args)
+        {
+            IntPtr lpData = Marshal.StringToHGlobalUni(args);
+
+            var cds = new COPYDATASTRUCT
+            {
+                dwData = IntPtr.Zero, // you can put your message type here
+                cbData = (args.Length) * 2, // Unicode bytes, + null terminator
+                lpData = lpData
+            };
+
+            SendMessage(handle, WM_COPYDATA, IntPtr.Zero, ref cds);
+
+            Marshal.FreeHGlobal(lpData);
+
+            Environment.Exit(0);
         }
 
         public static void Register()
@@ -73,20 +79,7 @@ namespace Avalonia.Maui.Platforms.Windows
             {
                 desktop.Startup += (_, _) =>
                 {
-                    Avalonia.Controls.Win32Properties.AddWndProcHookCallback(desktop.MainWindow,
-                             (IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, ref bool handled) =>
-                             {
-                                 if (msg == WM_COPYDATA)
-                                 {
-                                     MainThread.InvokeOnMainThreadAsync(async () =>
-                                     {
-                                         await Decode(lParam);
-                                     });
-                                     handled = true;
-                                 }
-
-                                 return IntPtr.Zero;
-                             });
+                    Avalonia.Controls.Win32Properties.AddWndProcHookCallback(desktop.MainWindow!, WndProc);
                 };
             }
         }
@@ -95,15 +88,40 @@ namespace Avalonia.Maui.Platforms.Windows
         {
             var cds = Marshal.PtrToStructure<COPYDATASTRUCT>(msg);
             string received = Marshal.PtrToStringUni(cds.lpData, cds.cbData / 2);
-            var arg = Encoding.Default.GetString(Convert.FromBase64String(received.Substring(AppActionPrefix.Length)));
+            var args = Decode(received);
             var actions = await AppActions.GetAsync();
-            var action = actions.FirstOrDefault(a => a.Id == arg);
+            var action = actions.FirstOrDefault(a => a.Id == args);
             if (action != null)
             {
-                var eventField = AppActions.Current.GetType().GetField("AppActionActivated", BindingFlags.Instance | BindingFlags.NonPublic);
+                RaiseOnAction(action);
+            }
+        }
 
-                var eventDelegate = (MulticastDelegate)eventField.GetValue(AppActions.Current);
+        private static string Decode(string args)
+        {
+            return Encoding.Default.GetString(Convert.FromBase64String(args.Substring(AppActionPrefix.Length)));
+        }
 
+        private static IntPtr WndProc(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_COPYDATA)
+            {
+                MainThread.InvokeOnMainThreadAsync(async () =>
+                {
+                    await Decode(lParam);
+                });
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private static void RaiseOnAction(AppAction action)
+        {
+            var eventField = AppActions.Current.GetType().GetField("AppActionActivated", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (eventField != null)
+            {
+                var eventDelegate = eventField.GetValue(AppActions.Current) as MulticastDelegate;
                 eventDelegate?.DynamicInvoke(AppActions.Current, new AppActionEventArgs(action));
             }
         }
