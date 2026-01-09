4b1d8e4e106a2346ac005c2af7bf3d196a02f4f2
Alexander Marek <alexander.marek@outlook.com>
2026-01-09 10:51:11 +0100

    - updated to Avalonia 12.x (added nuget.config for avalonia nightly feed) - updated project to .net10

---
+++ AvaloniaSample/AvaloniaSample.Android/Application.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Runtime;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.Maui;
+using CommunityToolkit.Maui;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace AvaloniaSample.Android;
+
+[Application]
+public class Application : AvaloniaAndroidApplication<App>
+{
+    protected Application(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+    {
+    }
+
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .UseMaui<AvaloniaSample.Maui.MauiApplication>(this, b => b
+                .ConfigureSyncfusionCore()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMediaElement());
+    }
+}
--- AvaloniaSample/AvaloniaSample.Android/MainActivity.cs
+++ AvaloniaSample/AvaloniaSample.Android/MainActivity.cs
@@ -1,10 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Avalonia;
 using Avalonia.Android;
-using Avalonia.Maui;
-using CommunityToolkit.Maui;
-using Syncfusion.Maui.Core.Hosting;
 
 namespace AvaloniaSample.Android;
 
@@ -14,14 +10,6 @@ namespace AvaloniaSample.Android;
     Icon = "@drawable/icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
+public class MainActivity : AvaloniaMainActivity
 {
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        return base.CustomizeAppBuilder(builder)
-            .UseMaui<AvaloniaSample.Maui.MauiApplication>(this, b => b
-                .ConfigureSyncfusionCore()
-                .UseMauiCommunityToolkit()
-                .UseMauiCommunityToolkitMediaElement());
-    }
 }
